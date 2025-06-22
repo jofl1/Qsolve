9987f9f8f06daf68e6eee41d5a3af618599b8d49
JoshFleming <joshyfleming15@icloud.com>
2025-06-22 14:20:03 +0100

    Cleanup

--- Qsolve/solvers/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-Advanced quantum solvers for Qsolve
-"""
-
-from .davidson import davidson, solve_davidson, benchmark_davidson_vs_eigsh
-
-__all__ = ['davidson', 'solve_davidson', 'benchmark_davidson_vs_eigsh']
